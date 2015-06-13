97511133ee0e78afebfe350552406bcce623733f
= <=>
2015-06-13 16:17:53 -0400

    feature: add Montreal as a playable map + add 2035 sanguinet building

---
+++ map/Montreal, QC/spawnpoints.lua
@@ -0,0 +1,22 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 0, worldY = 0, posX = 156, posY = 164, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 0, worldY = 0, posX = 156, posY = 164, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 0, worldY = 0, posX = 156, posY = 164, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 0, worldY = 0, posX = 156, posY = 164, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 0, worldY = 0, posX = 156, posY = 164, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 0, worldY = 0, posX = 156, posY = 164, posZ = 0 }
+  }
+}
+end
---
+++ map/Montreal, QC/worldobjects.lua
@@ -0,0 +1,7 @@
+objects = {
+  { name = "", type = "SpawnPoint", x = 156, y = 164, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "all"
+    }
+  }
+}
